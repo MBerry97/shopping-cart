e0a5048a1b0900435baa0a22178e31e13d3cad88
MBerry97 <imatty2011@hotmail.co.uk>
2020-11-23 15:11:06 +0000

    added grid display to cart and added template of the price display

---
+++ src/Components/PriceDisplay.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function PriceDisplay(props) {
+  return (
+    <div className='PriceDisplay'>
+      <span>Sub total: £100</span>
+      <span>Discount: £0.00</span>
+      <span>Est. Total: £100</span>
+    </div>
+  );
+}
+
+export default PriceDisplay;
--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import PriceDisplay from './PriceDisplay'
 
 function ShoppingCart(props) {
   return (
     <div className='ShoppingCart'>
-      
+      <PriceDisplay />
     </div>
   );
 }
