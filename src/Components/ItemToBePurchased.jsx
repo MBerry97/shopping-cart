6a6dfa9d741d439e9fa1c404c24093e6f086020a
MBerry97 <imatty2011@hotmail.co.uk>
2020-11-23 15:34:47 +0000

    added item display to the cart

---
+++ src/Components/ItemToBePurchased.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function ItemToBePurchased(props) {
+  return (
+    <div className='ItemToBePurchased-mainContainer'>
+      <div className='ItemToBePurchased-displayContainer'>
+        
+      </div>
+    </div>
+  );
+}
+
+export default ItemToBePurchased;
--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PriceDisplay from './PriceDisplay'
-
+import ItemToBePurchased from './ItemToBePurchased'
 function ShoppingCart(props) {
   return (
     <div className='ShoppingCart'>
       <PriceDisplay />
+      <ItemToBePurchased />
     </div>
   );
 }
