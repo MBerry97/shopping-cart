3cc703ff629dac8a58171ab373080980d27b85fb
MBerry97 <imatty2011@hotmail.co.uk>
2020-11-23 15:44:54 +0000

    item display has image of the item, item details and quantity

--- src/Components/ItemToBePurchased.jsx
+++ src/Components/ItemToBePurchased.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import item from '../assets/DJ-decks.jpg'
 
 
 function ItemToBePurchased(props) {
   return (
     <div className='ItemToBePurchased-mainContainer'>
       <div className='ItemToBePurchased-displayContainer'>
-        
+        <img src={item} alt='DJ-decks'></img>
+        <span>Pioneer XDJRX2 All In One DJ System for Rekordbox</span>
+        <span>Quantity: 1</span>
       </div>
     </div>
   );
