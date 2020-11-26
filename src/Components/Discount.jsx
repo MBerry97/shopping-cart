a6eda8625ab638ec0921b3c3e5b5dc0030513048
MBerry97 <imatty2011@hotmail.co.uk>
2020-11-26 21:54:33 +0000

    item img now has max-height

--- src/Components/Discount.jsx
+++ src/Components/Discount.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Discount = (props) => {
-
+  
 const activateDiscount = (event) => {
   event.preventDefault()
   const discountValue = event.target.discount.value
