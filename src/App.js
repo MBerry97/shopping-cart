4506998bda135c5a9ed07b511ed828affcd85939
MBerry97 <imatty2011@hotmail.co.uk>
2020-12-02 17:31:40 +0000

    added landing page with selection of items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import ShoppingCart from './Components/ShoppingCart'
 import Displayitems from './Components/DisplayItems'
+import {Router}from '@reach/router'
 
 function App() {
   return (
     <div className="App">
-      <Displayitems />
-      <ShoppingCart />
+      <Router>
+      <Displayitems path='/'/>
+      <ShoppingCart path='/checkout'/>
+      </Router>
     </div>
   );
 }
